ccae4f96e790f27bc619826345b5ce6877448a94
berz <berzellius@gmail.com>
2017-05-07 23:27:25 +0300

    дополнения в документацию

--- module/plate/config/documentation.config.php
+++ module/plate/config/documentation.config.php
@@ -1,8 +1,8 @@
 <?php
-return array(
-    'plate\\V1\\Rest\\Motion\\Controller' => array(
-        'collection' => array(
-            'GET' => array(
+return [
+    'plate\\V1\\Rest\\Motion\\Controller' => [
+        'collection' => [
+            'GET' => [
                 'description' => 'methods for motion of electro window',
                 'response' => '{
    "_links": {
@@ -35,63 +35,256 @@ return array(
        ]
    }
 }',
-            ),
-        ),
-    ),
-    'plate\\V1\\Rest\\Rooms\\Controller' => array(
+            ],
+        ],
+    ],
+    'plate\\V1\\Rest\\Rooms\\Controller' => [
         'description' => 'Сервис для получения списка комнат',
-        'collection' => array(
+        'collection' => [
             'description' => 'Список комнат',
-            'GET' => array(
-                'description' => 'Получить список комнат',
+            'GET' => [
+                'description' => 'Получить список комнат.
+Возвращает полный список комнат одинаково для любого авторизованного пользователя.',
                 'response' => '{
-   "_links": {
-       "self": {
-           "href": "/rooms"
-       },
-       "first": {
-           "href": "/rooms?page={page}"
-       },
-       "prev": {
-           "href": "/rooms?page={page}"
-       },
-       "next": {
-           "href": "/rooms?page={page}"
-       },
-       "last": {
-           "href": "/rooms?page={page}"
-       }
-   }
-   "_embedded": {
-       "rooms": [
-           {
-               "_links": {
-                   "self": {
-                       "href": "/rooms[/:rooms_id]"
-                   }
-               }
-              "room_id": "",
-              "name": ""
-           }
-       ]
-   }
+  "_links": {
+    "self": {
+      "href": "http://localhost:8080/rooms?page=1"
+    },
+    "first": {
+      "href": "http://localhost:8080/rooms"
+    },
+    "last": {
+      "href": "http://localhost:8080/rooms?page=1"
+    }
+  },
+  "_embedded": {
+    "rooms": [
+      {
+        "id": "1",
+        "floor_id": "1",
+        "name": "Гостиная",
+        "_links": {
+          "self": {
+            "href": "http://localhost:8080/rooms/1"
+          }
+        }
+      },
+      {
+        "id": "2",
+        "floor_id": "1",
+        "name": "Санузел#1",
+        "_links": {
+          "self": {
+            "href": "http://localhost:8080/rooms/2"
+          }
+        }
+      },
+      {
+        "id": "3",
+        "floor_id": "1",
+        "name": "Топка",
+        "_links": {
+          "self": {
+            "href": "http://localhost:8080/rooms/3"
+          }
+        }
+      },
+      {
+        "id": "4",
+        "floor_id": "1",
+        "name": "Кухня",
+        "_links": {
+          "self": {
+            "href": "http://localhost:8080/rooms/4"
+          }
+        }
+      },
+      {
+        "id": "5",
+        "floor_id": "1",
+        "name": "Столовая",
+        "_links": {
+          "self": {
+            "href": "http://localhost:8080/rooms/5"
+          }
+        }
+      },
+      {
+        "id": "6",
+        "floor_id": "1",
+        "name": "Веранда",
+        "_links": {
+          "self": {
+            "href": "http://localhost:8080/rooms/6"
+          }
+        }
+      },
+      {
+        "id": "7",
+        "floor_id": "2",
+        "name": "Спальня#1",
+        "_links": {
+          "self": {
+            "href": "http://localhost:8080/rooms/7"
+          }
+        }
+      },
+      {
+        "id": "8",
+        "floor_id": "2",
+        "name": "Санузел#2",
+        "_links": {
+          "self": {
+            "href": "http://localhost:8080/rooms/8"
+          }
+        }
+      },
+      {
+        "id": "9",
+        "floor_id": "2",
+        "name": "Спальня#2",
+        "_links": {
+          "self": {
+            "href": "http://localhost:8080/rooms/9"
+          }
+        }
+      },
+      {
+        "id": "10",
+        "floor_id": "2",
+        "name": "Коридор",
+        "_links": {
+          "self": {
+            "href": "http://localhost:8080/rooms/10"
+          }
+        }
+      },
+      {
+        "id": "11",
+        "floor_id": "3",
+        "name": "Спальня#3",
+        "_links": {
+          "self": {
+            "href": "http://localhost:8080/rooms/11"
+          }
+        }
+      },
+      {
+        "id": "12",
+        "floor_id": "3",
+        "name": "Санузел#3",
+        "_links": {
+          "self": {
+            "href": "http://localhost:8080/rooms/12"
+          }
+        }
+      }
+    ]
+  },
+  "page_count": 1,
+  "page_size": 25,
+  "total_items": 12,
+  "page": 1
 }',
-            ),
-        ),
-        'entity' => array(
+            ],
+        ],
+        'entity' => [
             'description' => 'Описание комнаты',
-            'GET' => array(
+            'GET' => [
                 'description' => 'Метод для получения данных о комнате',
                 'response' => '{
-   "_links": {
-       "self": {
-           "href": "/rooms[/:rooms_id]"
-       }
-   }
-   "room_id": "",
-   "name": ""
+  "id": "1",
+  "floor_id": "1",
+  "name": "Гостиная",
+  "_links": {
+    "self": {
+      "href": "http://localhost:8080/rooms/1"
+    }
+  }
 }',
-            ),
-        ),
-    ),
-);
+            ],
+        ],
+    ],
+    'plate\\V1\\Rest\\Floors\\Controller' => [
+        'description' => 'Сервис для получения списка этажей. Предоставляется только авторизованным пользователям, позволяет читать информацию об этажах здания.
+url: /floors[/:floors_id]',
+        'collection' => [
+            'description' => 'Метод позволяет получать список этажей.',
+            'GET' => [
+                'description' => 'Получить список этажей',
+                'response' => '{
+  "_links": {
+    "self": {
+      "href": "http://localhost:8080/floors?page=1"
+    },
+    "first": {
+      "href": "http://localhost:8080/floors"
+    },
+    "last": {
+      "href": "http://localhost:8080/floors?page=1"
+    }
+  },
+  "_embedded": {
+    "floors": [
+      {
+        "id": "1",
+        "name": "Первый этаж",
+        "_links": {
+          "self": {
+            "href": "http://localhost:8080/floors/1"
+          }
+        }
+      },
+      {
+        "id": "2",
+        "name": "Второй Этаж",
+        "_links": {
+          "self": {
+            "href": "http://localhost:8080/floors/2"
+          }
+        }
+      },
+      {
+        "id": "3",
+        "name": "Третий этаж",
+        "_links": {
+          "self": {
+            "href": "http://localhost:8080/floors/3"
+          }
+        }
+      },
+      {
+        "id": "4",
+        "name": "Четвертый этаж",
+        "_links": {
+          "self": {
+            "href": "http://localhost:8080/floors/4"
+          }
+        }
+      }
+    ]
+  },
+  "page_count": 1,
+  "page_size": 25,
+  "total_items": 4,
+  "page": 1
+}',
+            ],
+        ],
+        'entity' => [
+            'description' => 'Метод позволяет получить информацию по конкретному этажу.',
+            'GET' => [
+                'response' => '{
+  "id": "4",
+  "name": "Четвертый этаж",
+  "_links": {
+    "self": {
+      "href": "http://localhost:8080/floors/4"
+    }
+  }
+}',
+                'description' => 'Получить информацию по этажу.',
+            ],
+        ],
+    ],
+];
