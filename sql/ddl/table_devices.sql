e0364fb2a5b0ffbed8835ab4ecd85d1d769fbe90
Berzellius <berzellius@gmail.com>
2017-05-12 17:49:44 +0300

    обновление ddl: table_devices.sql

--- sql/ddl/table_devices.sql
+++ sql/ddl/table_devices.sql
@@ -5,15 +5,18 @@
 -- Структура таблицы `devices`
 --
 
-CREATE TABLE `devices` (
-  `id` int(11) NOT NULL,
-  `mac` varchar(40) NOT NULL,
-  `ip` varchar(15) NOT NULL,
-  `channel` int(11) NOT NULL,
-  `description` varchar(1000) NOT NULL,
-  `group_id` int(11) NOT NULL,
-  `room_id` int(11) NOT NULL,
-  `type` int(11) NOT NULL,
-  `max_amp` float NOT NULL,
-  `connection_type` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE devices
+(
+    id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    mac VARCHAR(40) NOT NULL,
+    ip VARCHAR(15) NOT NULL,
+    channel INT(11) NOT NULL,
+    description VARCHAR(1000) NOT NULL,
+    group_id INT(11) NOT NULL,
+    room_id INT(11) NOT NULL,
+    type INT(11) NOT NULL,
+    max_amp FLOAT NOT NULL,
+    connection_type INT(11) NOT NULL,
+    last_command VARCHAR(100)
+);
+CREATE UNIQUE INDEX ip_channel ON devices (ip, channel);
