c740af5400029f3b989c19173227bd8f928a4b6e
Berzellius <berzellius@gmail.com>
2017-06-04 23:15:31 +0300

    # тесты

--- module/plate/src/V1/Rpc/Devices_acl/Devices_aclControllerFactory.php
+++ module/plate/src/V1/Rpc/Devices_acl/Devices_aclControllerFactory.php
@@ -1,10 +1,15 @@
 <?php
 namespace plate\V1\Rpc\Devices_acl;
 
+use Interop\Container\ContainerInterface;
+use plate\V1\Rest\Devices\DevicesService;
+
 class Devices_aclControllerFactory
 {
-    public function __invoke($controllers)
+    public function __invoke(ContainerInterface $container)
     {
-        return new Devices_aclController();
+        /** @var DevicesService $devicesService */
+        $devicesService = $container->get(DevicesService::class);
+        return new Devices_aclController($devicesService);
     }
 }
